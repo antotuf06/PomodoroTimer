33e931810322ee081c4a9b5d0bca4cbd0fea267c
AntoTuf06 <antoniotufano206@gmail.com>
2026-09-18 21:18:37 +0200

    changed the GUI style; fixed the start button bug, close #1

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ const bellSound = document.getElementById("alarm-bell");
 
 let timeLeft = 25 * 60; // 25 minutes in seconds
 let interval;
+let isRunning = false;
 
 const updateTimerDisplay = () => {
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
   timer.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
+
 const startTimer = () => {
+    if(isRunning) return; //if the timer is already running, do nothing
+    
+    isRunning = true;
+    document.getElementById("start").disabled = true; //disable the start button while the timer is running
     interval = setInterval(() => {      //every second, decrease the time left and update the display
         timeLeft--;
         updateTimerDisplay();
 
         if(timeLeft == 0)                //if the timer reaches 0, stop the timer, alert the user and ask if they want to start a break or a new pomodoro
         {
+            isRunning = false;
             clearInterval(interval);
             bellSound.play();
             if(confirm("Time's up! Do you want to start a 5-minute break?")) {
@@ -36,12 +43,16 @@ const startTimer = () => {
 }
 
 const stopTimer = () => {
+    isRunning = false;
+    document.getElementById("start").disabled = false; //enable the start button when the timer is stopped
     clearInterval(interval);
     alert("Timer stopped!");
 
 }
 
 const resetTimer = () => {
+    isRunning = false;
+    document.getElementById("start").disabled = false; //enable the start button when the timer is reset
     clearInterval(interval);
     alert("Timer reset!");
     timeLeft = 25 * 60;
